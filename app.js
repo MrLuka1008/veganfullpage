4098c4c1215f18bc7e3c1c3fd42116cd4a4691f1
MrLuka1008 <lukadigmelashvili@gmail.com>
2022-07-21 14:21:06 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+var countDownDate = new Date("jul 30, 2022 15:37:25").getTime();
+
+var x = setInterval(function () {
+  var now = new Date().getTime();
+
+  var distance = countDownDate - now;
+
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("time").innerHTML =
+    days + " &#58;" + hours + "&#58; " + minutes + "&#58; " + seconds;
+}, 1000);
